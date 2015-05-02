0025a1452337884627b14fe8b004fbb422c55224
DANIEMB <DANIEMB@EMB-PC>
2015-05-02 12:35:56 +0700

    class commit

--- src/inventanity/RuangKelas.java
+++ src/inventanity/RuangKelas.java
@@ -3,9 +3,15 @@ package inventanity;
 
 public class RuangKelas {
 	private String NamaRuang, Lokasi, Fakultas, Prodi;
+	private String KondisiSteker, KondisiKblLCD, KondisiLamp, KondisiAC;
+	private String KondisiLante, KondisiDinding, KondisiAtap, KondisiPintu, KondisiJendela;
+	private String SirkulaUdara;
 	private double Panjang, Lebar, Luas;
+	private double PosisiSteker, PosisiKblLCD, PosisiLamp, PosisiAC;
 	private double JumlahPintu, JumlahJendela, JumlahKursi, JumlahSteker;
 	private double JmlKabelLCD, JumlahLampu, JumlahAC;
+	private double NilaiCahaya, KeLembapan, Suhu, KeBisingan, Bau, Bocor, KeRusakan, KeAusan;
+	private double KeKokohan, PintuBkunci, JendelaBkunci, Bahaya;
 
 	double HitungLuas() {
 		return Panjang * Lebar;
@@ -125,6 +131,107 @@ public class RuangKelas {
 
 	public void setJumlahAC(double jumlahAC) {
 		JumlahAC = jumlahAC;
+	}
+	public String getKondisiSteker() {
+		return KondisiSteker;
+	}
+
+	public String getKondisiKblLCD() {
+		return KondisiKblLCD;
+	}
+
+	public String getKondisiLamp() {
+		return KondisiLamp;
+	}
+
+	public String getKondisiAC() {
+		return KondisiAC;
+	}
+	public double getPosisiSteker() {
+		return PosisiSteker;
+	}
+
+	public double getPosisiKblLCD() {
+		return PosisiKblLCD;
+	}
+
+	public double getPosisiLamp() {
+		return PosisiLamp;
+	}
+
+	public double getPosisiAC() {
+		return PosisiAC;
+	}
+	public String getKondisiLante() {
+		return KondisiLante;
+	}
+
+	public String getKondisiDinding() {
+		return KondisiDinding;
+	}
+
+	public String getKondisiAtap() {
+		return KondisiAtap;
+	}
+
+	public String getKondisiPintu() {
+		return KondisiPintu;
+	}
+
+	public String getKondisiJendela() {
+		return KondisiJendela;
+	}
+
+	public String getSirkulaUdara() {
+		return SirkulaUdara;
+	}
+
+	public double getNilaiCahaya() {
+		return NilaiCahaya;
+	}
+
+	public double getKeLembapan() {
+		return KeLembapan;
+	}
+
+	public double getSuhu() {
+		return Suhu;
+	}
+
+	public double getKeBisingan() {
+		return KeBisingan;
+	}
+
+	public double getBau() {
+		return Bau;
+	}
+
+	public double getBocor() {
+		return Bocor;
+	}
+
+	public double getKeRusakan() {
+		return KeRusakan;
+	}
+
+	public double getKeAusan() {
+		return KeAusan;
+	}
+
+	public double getKeKokohan() {
+		return KeKokohan;
+	}
+
+	public double getPintuBkunci() {
+		return PintuBkunci;
+	}
+
+	public double getJendelaBkunci() {
+		return JendelaBkunci;
+	}
+
+	public double getBahaya() {
+		return Bahaya;
 	}
 
 }
