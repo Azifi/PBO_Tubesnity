165b306595885e64b2b7d0f1a8c4c985133147e5
AZIFI <AZIFI@MAIWEI>
2015-05-10 20:31:20 +0700

    class commit

--- src/triality_code/RkelasMain.java
+++ src/triality_code/RkelasMain.java
@@ -3,21 +3,23 @@ package triality_code;
 import java.util.Scanner;
 
 public class RkelasMain {
+	private static Scanner pufft;
+
 	public static void main(String[] auch){
-		Scanner pufft = new Scanner (System.in);
-		PropertiesRkelas nampil = new PropertiesRkelas(); 
-		RuangKelas Oper = new RuangKelas();
+		pufft = new Scanner (System.in);
+		MasukanData masukin = new MasukanData();
+		masukin.IdentitasRkelas();
+		masukin.KondisiRkelas();
+		masukin.SaranaRkelas();
+		masukin.KenyamananKelas();
+		masukin.KeamananKelas();
 		
-		System.out.println("Masukan Panjang : ");
-		Oper.setPanjang(pufft.nextInt());
-		System.out.println("Masukan Lebar : ");
-		Oper.setLebar(pufft.nextInt());
-		System.out.println("Masukan Jumlah Kursi : ");
-		Oper.setJmlKursi(pufft.nextInt());
+		TampilkanData oper = new TampilkanData();
+		oper.TampilkanIdentitas();
+		oper.TampilkanKondisi();
+		oper.TampilkanSarana();
+		oper.TampilKenyamanan();
+		oper.TampilKeamanan();		
 		
-		System.out.println(nampil.HitungLuas(Oper.getPanjang(), Oper.getLebar()));
-		System.out.println(nampil.BentukRuang(Oper.getPanjang(), Oper.getLebar()));
-		System.out.println(nampil.HitungRasio(Oper.getLuas(), Oper.getJmlKursi(),Oper.getPanjang(), Oper.getLebar()));
-		System.out.println(nampil.HitungRasionya(Oper.getLuas(), Oper.getJmlKursi()));
 	}
 }
